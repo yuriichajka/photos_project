edc6a95ab3f91fbdc19a8ebdbe24773f3fe1c95b
yuriichajka <yuriichajka@gmail.com>
2021-08-02 18:33:52 +0300

    02.08.2021 start project pictures list, hardcode modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Modal from "./components/Modal";
+import {GlobalStyles} from "./globalStyles";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 20px;
+  text-align: center;
+`
+
 
 function App() {
+
+  let [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch('https://boiling-refuge-66454.herokuapp.com/images')
+        .then(value => value.json())
+        .then(value => {
+          setImages(value)
+        })
+  }, [])
+
+
+    let [showModal, setShowModal] = useState(false);
+    const openModal = (e) => {
+        setShowModal(prev => !prev )
+        console.log(e)
+    }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Container>
+      {
+        images.map(value => <div>
+          <img onClick={() => openModal(value.url)} src={value.url} alt='da'/>
+        </div>)
+      }
+
+            <Modal showModal={showModal} setShowModal={setShowModal} />
+            <GlobalStyles/>
+        </Container>
+    </>
   );
 }
 
---
+++ src/components/Modal.js
@@ -0,0 +1,110 @@
+import React from "react";
+import styled from "styled-components";
+import {MdClose} from 'react-icons/md'
+
+const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const ModalWrapper = styled.div`
+    width: 800px;
+    height: 500px;
+    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+    color: #000;
+    background: #fff;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    position: relative;
+    z-index: 10;
+    border-radius: 10px;
+`
+
+const ModalContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+    align-items: center;
+    line-height: 1.8;
+    color: #141414;
+  
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+    width: 75%;
+    margin-top: 10px;
+  }
+  
+  input {
+    width: 75%;
+    margin: 10px 0;
+    padding: 5px;
+  }
+  
+  img {
+    margin-bottom: 20px;
+  }
+`
+
+const CloseModalButton = styled(MdClose)`
+    cursor: pointer;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 32px;
+    height: 32px;
+    padding: 0;
+    z-index: 10;
+`
+
+const Comments = styled.div`
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  
+  p {
+    text-align: left;
+    margin-top: 10px;
+  }
+  
+  div {
+    text-align: left;
+    color: darkgray;
+  }
+`
+
+
+export default function Modal({showModal, setShowModal}) {
+
+    return (
+        <>
+            {showModal ? (
+                <Background>
+                    <ModalWrapper showModal={showModal}>
+                        <ModalContent>
+                            <img src='https://picsum.photos/id/240/300/200' alt="pic" width='400px'/>
+                            <input type="text" placeholder='Ваше имя'/>
+                            <input type="text" placeholder='Ваш комментарий'/>
+                            <button>Оставить отзыв</button>
+                        </ModalContent>
+                        <CloseModalButton aria-label='Close modal' onClick={() => setShowModal(prev => !prev)}/>
+                        <Comments>
+                            <div>Lorem ipsum.</div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, saepe.</p>
+                            <br/>
+                            <div>Lorem ipsum.</div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, saepe.</p>
+                        </Comments>
+                    </ModalWrapper>
+                </Background>
+            ) : null}
+        </>
+    );
+};
